2d2f007bf4488745bbe265f4fce9e638ed4cec2a
Fabio <35787819+wdvlpr@users.noreply.github.com>
2022-09-16 13:44:29 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,6 @@
 var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
+  console.log('001');
   e.waitUntil(
    caches.open(staticCacheName).then(function (cache) {
     return cache.addAll(["/"]);
@@ -8,6 +9,7 @@ self.addEventListener("install", function (e) {
 });
 
 self.addEventListener("fetch", function (event) {
+  console.log('002');
   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function (response) {
